fe574c69b9c9066a26b11adadb2e1c16d6a984e2
bartholomaeuss <bartholomaeus.s@googlemail.com>
2021-01-17 19:39:49 +0100

    new

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -3,7 +3,7 @@ Lines = file1.readlines()
   
 
 
-def part_01(right: int, down: int):
+def part_01_02(right: int, down: int):
     count_r = 0
     count_d = 0
     trees = 0
@@ -13,7 +13,7 @@ def part_01(right: int, down: int):
         d = count_d % down
         length = line.__len__()
         t = (r / length) + 1
-        t = t.__int__()
+        t = int(t)
         rr = r % length
         count_d = count_d + 1
         if d!=0:
@@ -21,16 +21,13 @@ def part_01(right: int, down: int):
         count_r = count_r + 1
         if line[rr]=="#":
             trees = trees + 1
-        #print(line)
-        #print(line[rr])
-        #print(" "*rr + line[rr])
     return trees
     
-print(part_01(1, 1))
-print(part_01(3, 1))
-print(part_01(5, 1))
-print(part_01(7, 1))
-print(part_01(1, 2))
-product = part_01(1, 1) * part_01(3, 1) * part_01(5, 1) * part_01(7, 1) * \
-    part_01(1, 2)
+print(part_01_02(1, 1))
+print(part_01_02(3, 1))
+print(part_01_02(5, 1))
+print(part_01_02(7, 1))
+print(part_01_02(1, 2))
+product = part_01_02(1, 1) * part_01_02(3, 1) * part_01_02(5, 1) *\
+    part_01_02(7, 1) * part_01_02(1, 2)
 print(product)
